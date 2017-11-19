55659e1e0fd1d2219e691957ca0fd9179926d687
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-11-19 23:47:51 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pycmark',
+    version='0.1.0',
+    url='https://github.com/tk0miya/pycmark',
+    license='BSD',
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya@gmail.com',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Documentation',
+    ],
+    install_requires=[
+        'docutils',
+    ],
+    extras_require={
+        'test': [
+            'flake8',
+            'pytest',
+            'mypy',
+        ],
+    },
+    platforms='any',
+    packages=find_packages(),
+)

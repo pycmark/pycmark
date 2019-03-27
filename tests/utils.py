6cd1b456d1b89f10ecc45b5b97f32aae883a20f5
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-03-27 19:55:12 +0900

    Fix license of assert_node()

--- tests/utils.py
+++ tests/utils.py
@@ -12,6 +12,8 @@ from docutils.readers.standalone import Reader
 from pycmark import CommonMarkParser
 from pycmark.transforms import LinebreakFilter
 
+from sphinx import assert_node  # NOQA
+
 
 class TestReader(Reader):
     def get_transforms(self):
@@ -30,33 +32,3 @@ def publish(text):
                            source_path='dummy.md',
                            reader=TestReader(),
                            parser=TestParser())
-
-
-# copied from sphinx/testing/util.py
-def assert_node(node, cls=None, xpath="", **kwargs):
-    if cls:
-        if isinstance(cls, list):
-            assert_node(node, cls[0], xpath=xpath, **kwargs)
-            if cls[1:]:
-                if isinstance(cls[1], tuple):
-                    assert_node(node, cls[1], xpath=xpath, **kwargs)
-                else:
-                    assert len(node) == 1, \
-                        'The node%s has %d child nodes, not one' % (xpath, len(node))
-                    assert_node(node[0], cls[1:], xpath=xpath + "[0]", **kwargs)
-        elif isinstance(cls, tuple):
-            assert len(node) == len(cls), \
-                'The node%s has %d child nodes, not %r' % (xpath, len(node), len(cls))
-            for i, nodecls in enumerate(cls):
-                path = xpath + "[%d]" % i
-                assert_node(node[i], nodecls, xpath=path, **kwargs)
-        elif isinstance(cls, str):
-            assert node == cls, 'The node %r is not %r: %r' % (xpath, cls, node)
-        else:
-            assert isinstance(node, cls), \
-                'The node%s is not subclass of %r: %r' % (xpath, cls, node)
-
-    for key, value in kwargs.items():
-        assert key in node, 'The node%s does not have %r attribute: %r' % (xpath, key, node)
-        assert node[key] == value, \
-            'The node%s[%s] is not %r: %r' % (xpath, key, value, node[key])
